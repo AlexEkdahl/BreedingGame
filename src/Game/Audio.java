d2377d8755f940cbb1a01ae0c6bc2f49a8bac9f4
AlexEkdahl <alexanderekdahl@me.com>
2021-01-22 15:41:52 +0100

    Added Manifest.txt and runnable Game.jar

---
+++ src/Game/Audio.java
@@ -0,0 +1,22 @@
+package Game;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+
+public class Audio implements Serializable {
+    private static final long serialVersionUID = 1L;
+    static Clip clip;
+    static AudioInputStream stream;
+
+    public Audio(String filePath)
+            throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+        stream = AudioSystem.getAudioInputStream(new File(filePath));
+        clip = AudioSystem.getClip();
+        clip.open(stream);
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+        clip.start();
+    }
+
+}
--- src/Game/Main.java
+++ src/Game/Main.java
@@ -2,6 +2,7 @@ package Game;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        new Audio("audio/themesong.wav");
         GameHelper.clearScreen();
         // just for debugging methods
         new Game();
