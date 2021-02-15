4813fed7f830ec00341e4319bfe111e6fcda10f2
Alexander Ekdahl <77007088+AlexEkdahl@users.noreply.github.com>
2021-02-15 15:27:42 +0100

    Update Game.java

--- src/game/Audio.java
+++ src/game/Audio.java
@@ -8,6 +8,7 @@ import java.io.Serializable;
 public class Audio implements Serializable {
 
     static Clip clip;
+    static Clip clip1;
     static AudioInputStream stream;
 
     public static void soundEffect(String filePath) throws IOException, UnsupportedAudioFileException,
@@ -21,22 +22,22 @@ public class Audio implements Serializable {
     public static void themeSong(String filePath) throws IOException, UnsupportedAudioFileException,
             LineUnavailableException {
         stream = AudioSystem.getAudioInputStream(new File(filePath));
-        clip = AudioSystem.getClip();
-        clip.open(stream);
-        FloatControl volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        clip1 = AudioSystem.getClip();
+        clip1.open(stream);
+        FloatControl volume = (FloatControl) clip1.getControl(FloatControl.Type.MASTER_GAIN);
         double sound = 0.2;
         // calculate db
         float dB = (float) (Math.log(sound) / Math.log(10.0) * 20.0);
         volume.setValue(dB);
-        clip.loop(Clip.LOOP_CONTINUOUSLY);
-        clip.start();
+        clip1.loop(Clip.LOOP_CONTINUOUSLY);
+        clip1.start();
     }
 
     public static void dummyMethod() {
-        if (clip.isRunning()) {
-            clip.stop();
+        if (clip1.isRunning()) {
+            clip1.stop();
         } else {
-            clip.start();
+            clip1.start();
         }
     }
 
